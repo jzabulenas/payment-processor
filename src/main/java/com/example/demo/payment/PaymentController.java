962155b47089cb0ce906617f95e105768ba159f7
Julius <jzabulenas@protonmail.com>
2025-06-11 11:30:08 +0300

    Add @RequestBody to createPayment controller method

--- src/main/java/com/example/demo/payment/PaymentController.java
+++ src/main/java/com/example/demo/payment/PaymentController.java
@@ -3,6 +3,7 @@ package com.example.demo.payment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class PaymentController {
   }
 
   @PostMapping("/payments")
-  public ResponseEntity<Payment> createPayment(Payment payment) {
+  public ResponseEntity<Payment> createPayment(@RequestBody Payment payment) {
     Payment savedPayment = this.paymentRepository.save(payment);
 
     return ResponseEntity.ok(savedPayment);
