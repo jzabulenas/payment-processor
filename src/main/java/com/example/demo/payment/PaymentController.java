9204c67fc5b30f4501440d52a6014cac1be815cc
Julius <jzabulenas@protonmail.com>
2025-06-11 11:21:24 +0300

    Add PaymentController

---
+++ src/main/java/com/example/demo/payment/PaymentController.java
@@ -0,0 +1,26 @@
+package com.example.demo.payment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PaymentController {
+
+  private final PaymentRepository paymentRepository;
+
+  @Autowired
+  public PaymentController(PaymentRepository paymentRepository) {
+    this.paymentRepository = paymentRepository;
+  }
+
+  @PostMapping("/payments")
+  public ResponseEntity<Payment> createPayment(Payment payment) {
+    Payment savedPayment = this.paymentRepository.save(payment);
+
+    return ResponseEntity.ok(savedPayment);
+  }
+}
