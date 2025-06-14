77e9f36e431e6626733653f1b2621ee1e780c636
Julius <jzabulenas@protonmail.com>
2025-06-14 23:00:03 +0300

    Add cancelPayment controller method

--- src/main/java/com/example/demo/payment/PaymentController.java
+++ src/main/java/com/example/demo/payment/PaymentController.java
@@ -2,7 +2,9 @@ package com.example.demo.payment;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,4 +35,10 @@ public class PaymentController {
         .body(paymentResponseDTO);
   }
 
+  @PutMapping("/payments/{id}/cancel")
+  public ResponseEntity<PaymentResponseDTO> cancelPayment(@PathVariable long id) {
+    PaymentResponseDTO paymentResponseDTO = this.paymentService.cancelPayment(id);
+
+    return ResponseEntity.ok(paymentResponseDTO);
+  }
 }
